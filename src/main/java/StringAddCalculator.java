e9bd541bd7c32d2f8e8a6f666ab607f3280539f2
sangyounkim <505>
2020-06-28 16:13:46 +0900

    fix : 하드코딩 및 값 비싼 Pattern 객체 상수화 feat : validate 로직 분리.

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,16 +1,26 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * 역할 : 문자열을 덧셈해주는 객체..
+ */
 public class StringAddCalculator {
+
+    private static final String SPLIT_TEXT_REGX = ",|:";
+    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
+    private StringAddCalculator() {
+
+    }
 
     public static int splitAndSum(String parameter) {
         int result = 0;
 
-        if (parameter == null || parameter.isEmpty()) {
-            return result;
+        if(validate(parameter)) {
+            return 0;
         }
 
-        String[] paramArray = makeStringArray(parameter);
+        String[] paramArray = splitText(parameter);
 
         for (int i = 0; i < paramArray.length; i++) {
             result = calculate(result , Integer.parseInt(paramArray[i]));
@@ -18,15 +28,15 @@ public class StringAddCalculator {
         return result;
     }
 
-    public static String[] makeStringArray(String parameter) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(parameter);
+    public static String[] splitText(String text) {
+        Matcher m = pattern.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
 
             return tokens;
         }
-        return parameter.split(",|:");
+        return text.split(SPLIT_TEXT_REGX);
     }
 
     public static int calculate(int currNum, int otherNum) {
@@ -36,4 +46,11 @@ public class StringAddCalculator {
         return currNum + otherNum;
     }
 
+    public static boolean validate(String parameter) {
+
+        if (parameter == null || parameter.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
